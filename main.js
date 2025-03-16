07fd7d820fb85fe17411204260251c8d6f5de89c
HunterxDestiny <mailpourtwitchcarcabug@gmail.com>
2025-03-16 21:49:19 +0100

    feat(app)first-commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const createButton = document.getElementById('create-item');
+
+createButton.addEventListener('click', () => {
+    const menuOverlayer = document.createElement('div');
+    const menu = document.createElement('section');
+
+})
